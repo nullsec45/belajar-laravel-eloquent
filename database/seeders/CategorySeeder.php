b8546cf04ecc42223ede01a3ee0f2d748c344aaf
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-08-19 14:52:47 +0700

    CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $category=new Category();
+        $category->id="FOOD";
+        $category->name="Food";
+        $category->description="Food Category";
+        $category->save();
     }
 }
