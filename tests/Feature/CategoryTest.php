77649fd11bf3b421f2800c7db83ba46a08915451
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-08-19 14:52:22 +0700

    insert many, find, update, select, update many, delete, delete many,

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Category;
+use Database\Seeders\CategorySeeder;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -17,5 +18,97 @@ class CategoryTest extends TestCase
         $result=$category->save();
 
         self::assertTrue($result);
+    }
+
+    public function testInsertMany(){
+        $categories=[];
+
+        for($i=0;$i<10;$i++){
+            $categories[]=[
+                "id" => "ID $i",
+                "Name" => "Name $i"
+            ];
+        }
+
+        $result=Category::insert($categories);
+
+        $total=Category::count();
+        
+        self::assertEquals(10, $total);
+    }
+
+    public function testFind(){
+        $this->seed(CategorySeeder::class);
+
+        $category=Category::find("FOOD");
+        self::assertNotNull($category);
+        self::assertEquals("FOOD", $category->id);
+        self::assertEquals("Food", $category->name);
+        self::assertEquals("Food Category", $category->description);
+    }
+
+    public function testUpdate(){
+        // $this->seed(CategorySeed::class);
+
+        $category=Category::find("FOOD");
+        $category->name="Food Updated";
+        
+        $result=$category->update();
+        self::assertTrue($result);
+    }
+
+    public function testSelect(){
+        for($i=0;$i < 5;$i++){
+            $category=new Category();
+            $category->id="$i";
+            $category->name="Category $i";
+            $category->save();
+        }
+
+        $categories=Category::whereNull("description")->get();
+        self::assertEquals(15, $categories->count());
+
+        $categories->each(function($category){
+            self::assertNull($category->description);
+        });
+    }
+
+    public function testUpdateMany(){
+        Category::whereNull("description")->update([
+            "description" => "Updated"
+        ]);
+        $total=Category::where("description","=","Updated")->count();
+        self::assertEquals(15, $total);
+    }
+
+    public function testDelete(){
+        $category=Category::where("description","Updated");
+        $result=$category->delete();
+
+        self::assertTrue($result);
+        $total=Category::count();
+
+        self::assertEquals(2, $total);
+    }
+
+    public function testDeleteMany(){
+        $categories=[];
+
+        for($i=0;$i < 10;$i++){
+            $categories[]=[
+                "id" => $i,
+                "name" => "Category $i"
+            ];
+        }
+        $result=Category::insert($categories);
+        self::assertTrue($result);
+
+        $total=Category::count();
+        self::assertEquals(12,$total);
+
+        Category::whereNull("description")->delete();
+        $total=Category::count();
+
+        self::assertEquals(2, $total);
     }
 }
