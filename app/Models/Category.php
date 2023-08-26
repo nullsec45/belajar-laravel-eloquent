6981ddd2eb9048ea065017c1ac02daba9be0155c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-08-26 10:25:27 +0700

    global scope dan fillable

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Scopes\IsActiveScope;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
-    protected $table="categories", $primaryKey="id", $keyType="string";
+    protected $table="categories", 
+              $primaryKey="id",
+              $keyType="string",
+              $fillable=["id","name","description"];
 
     public $incrementing=false, $timestamps=false;
+
+    protected static function booted():void{
+        parent::booted();
+        self::addGlobalScope(new IsActiveScope());
+    }
 }
