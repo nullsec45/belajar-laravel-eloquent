69cc1723a3525aed1525fdb25308fe78aa82498e
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-08-26 10:30:44 +0700

    test Soft Delete

--- tests/Feature/VoucherTest.php
+++ tests/Feature/VoucherTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\Voucher;
 use Illuminate\Support\Facades\DB;
+use Database\Seeders\VoucherSeeder;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -28,5 +29,18 @@ class VoucherTest extends TestCase
 
         self::assertNotNull($voucher->id);
         self::assertNotNull($voucher->voucher_code);
+    }
+
+    public function testSoftDelete(){
+        $this->seed(VoucherSeeder::class);
+
+        $voucher=Voucher::query()->where("name","Sample Voucher 3")->first();
+        $voucher->delete();
+
+        $voucher=Voucher::query()->where("name","Sample Voucher 3")->first();
+        self::assertNull($voucher);
+
+        $voucher=Voucher::withTrashed()->where("name","Sample Voucher 3")->first();
+        self::assertNotNull($voucher);
     }
 }
