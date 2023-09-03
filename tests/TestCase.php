ce2f7f993de903ce984c8e6d6b209914fb77b723
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-03 17:20:22 +0700

    Delete DB in setUp TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp():void{
+        parent::setUp();
+
+        DB::delete("DELETE FROM categories");
+        DB::delete("DELETE FROM vouchers");
+        DB::delete("DELETE FROM wallets");
+        DB::delete("DELETE FROM customers");
+    }
 }
