326fbbac3008972b896183b50ae3a6dc31c2136e
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-03 17:19:28 +0700

    wallet table migration

---
+++ database/migrations/2023_09_03_053609_create_wallets_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->integerIncrements("id");
+            $table->string("customer_id", 100);
+            $table->bigInteger("amount")->default(0);
+            $table->foreign("customer_id")->references("id")->on("customers");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
