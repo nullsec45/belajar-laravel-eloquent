67eeca436324a50e6903c20b0da08be07ea0be7d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-17 11:51:48 +0700

    Category Seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,9 +14,9 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $category=new Category();
-        $category->id="MUSIC";
-        $category->name="Music";
-        $category->description="Music Category";
+        $category->id="FOOD";
+        $category->name="Food";
+        $category->description="Food Category";
         $category->is_active=true;
         $category->save();
     }
