7ef41f2e0a6d53ef10d0ed13a898e3f6bfeb5a8a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-20 12:29:05 +0700

    new :Image model

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    protected $table="images",
+              $primaryKey="id",
+              $keyType="int";
+
+    public $incrementing=true,
+           $timestamps=false;
+
+    
+    public function imageable():MorphTo{
+            return $this->morphTo();
+    }
+
 }
