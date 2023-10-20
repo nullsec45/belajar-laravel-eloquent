0ff1cc2bf080f56f0efe783cdde70e2c999a80d0
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-20 12:31:06 +0700

    Image seeder create to db

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\Customer;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ImageSeeder extends Seeder
 {
@@ -12,6 +15,20 @@ class ImageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       {
+        $image=new Image();
+        $image->url="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/985px-Laravel.svg.png";
+        $image->imageable_id="FAJAR";
+        $image->imageable_type=Customer::class;
+        $image->save();
+       }
+       {
+        $image=new Image();
+        $image->url="https://w7.pngwing.com/pngs/866/528/png-transparent-php-web-development-perl-logo-php-logo-cdr-text-trademark-thumbnail.png";
+        $image->imageable_id="1";
+        $image->imageable_type=Product::class;
+        $image->save();
+       }
+
     }
 }
