0dd5ba1061c461e6093f2025b9139df40ea019d5
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-20 12:31:19 +0700

    TestCase:delete table images

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,6 +13,7 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         DB::delete("DELETE FROM reviews");
+        DB::delete("DELETE FROM images");
         DB::delete("DELETE FROM customers_likes_products");
         DB::delete("DELETE FROM products");
         DB::delete("DELETE FROM categories");
