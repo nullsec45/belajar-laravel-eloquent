11548790b37bbd5abeace8fa4b61703b87e972b2
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-22 11:40:36 +0700

    ProductTest

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -5,8 +5,11 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\Product;
 use App\Models\Category;
+use Database\Seeders\TagSeeder;
 use Database\Seeders\ImageSeeder;
+use Database\Seeders\CommentSeeder;
 use Database\Seeders\ProductSeeder;
+use Database\Seeders\VoucherSeeder;
 use Database\Seeders\CategorySeeder;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -48,5 +51,50 @@ class ProductTest extends TestCase
         $image=$product->image;
         self::assertNotNull($image);
         self::assertEquals("https://w7.pngwing.com/pngs/866/528/png-transparent-php-web-development-perl-logo-php-logo-cdr-text-trademark-thumbnail.png", $image->url);
+    }
+
+    public function testOneToManyPolymorphic(){
+        $this->seed([CategorySeeder::class, ProductSeeder::class, VoucherSeeder::class, CommentSeeder::class]);
+
+        $product=Product::find("1");
+        self::assertNotNull($product);
+
+        $comments=$product->comments;
+        foreach($comments as $comment){
+            self::assertEquals(Product::class, $comment->commentable_type);
+            self::assertEquals($product->id, $comment->commentable_id);
+        }
+    }
+
+    public function testOneOfManyPolymorphic(){
+        $this->seed([CategorySeeder::class, ProductSeeder::class, VoucherSeeder::class, CommentSeeder::class]);
+
+        $product=Product::find("1");
+        self::assertNotNull($product);
+
+        $lastComment=$product->latestComment();
+        self::assertEquals($lastComment);
+
+        $oldestComment=$product->oldestComment();
+        self::assertEquals($oldestComment);
+    }
+
+    public function testManyToManyPolymorphic(){
+        $this->seed([CategorySeeder::class, ProductSeeder::class, VoucherSeeder::class, CommentSeeder::class, TagSeeder::class]);
+        
+        $product=Product::find("1");
+        $tags=$product->tags;
+
+        self::assertNotNull($tags);
+        self::assertCount(1, $tags);
+
+        foreach($tags as $tag){
+            self::assertNotNull($tag->id);
+            self::assertNotNull($tag->name);
+
+            $vouchers=$tag->vouchers;
+            self::assertNotNull($vouchers);
+            self::assertCount(1, $vouchers);
+        }
     }
 }
