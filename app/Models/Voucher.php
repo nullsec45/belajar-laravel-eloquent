e3227d0fe24fbcf0c6154baddb3410cdf372b287
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-22 11:38:49 +0700

    relation morphToMany

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -35,4 +35,8 @@ class Voucher extends Model
     public function comments(){
         return $this->morphMany(Comment::class,"commentable");
     }
+
+    public function tags(){
+        return $this->morphToMany(Tag::class, "taggable");
+    }
 }
