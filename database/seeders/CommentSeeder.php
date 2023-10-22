c91c6f8c1e7b8036039060a74d1a7b690cb95a88
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-22 11:39:47 +0700

    cretae comment seeder to isnert db

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
+use App\Models\Product;
+use App\Models\Voucher;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CommentSeeder extends Seeder
 {
@@ -12,6 +15,26 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $this->createCommentsForProduct();
+    }
+
+    private function createCommentsForProduct():void{
+        $product=Product::find("1");
+
+        $comment=new Comment();
+        $comment->email="ramafajar805@gmail.com";
+        $comment->title="Comment Title";
+        $comment->commentable_id=$product->id;
+        $comment->commentable_type="product";
+    }
+
+    private function createCommentsForVoucher():void{
+        $product=Voucher::first();
+
+        $comment=new Comment();
+        $comment->email="ramafajar805@gmail.com";
+        $comment->title="Comment Title";
+        $comment->commentable_id=$product->id;
+        $comment->commentable_type="voucher";
     }
 }
