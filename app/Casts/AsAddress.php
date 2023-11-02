42858c48b3145f6f1eea3f06b9f11bb51de15efd
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-03 06:11:54 +0700

    new:casts AsAddress

---
+++ app/Casts/AsAddress.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Casts;
+
+use App\Models\Address;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class AsAddress implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function get(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        if($value == null) return null;
+
+        $addresses=explode(", ", $value);
+        
+        return new Address($addresses[0], $addresses[1], $addresses[2], $addresses[3]);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function set(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+       if($value instanceof Address){
+            return "$value->street, $value->city, $value->country, $value->postalCode";
+       }else{
+            return null;
+       }
+    }
+}
