b4cb8737ae8c5b46cad001dd81aa7bedf234ede1
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-03 06:13:40 +0700

    new:import models Address to PersonTest and testCustomCasts

--- tests/Feature/PersonTest.php
+++ tests/Feature/PersonTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Person;
+use App\Models\Address;
 use Illuminate\Support\Carbon;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -47,5 +48,21 @@ class PersonTest extends TestCase
         self::assertNotNull($person->updated_at);
         self::assertInstanceOf(Carbon::class, $person->created_at);
         self::assertInstanceOf(Carbon::class, $person->updated_at);
+    }
+
+    public function testCustomCasts(){
+        $person=new Person();
+        $person->first_name="Rama";
+        $person->last_name="Fajar";
+        $person->address=new Address("Jalan Permata 1","Jakarta","Indonesia","13320");
+        $person->save();
+
+        $person=Person::query()->find($person->id);
+        self::assertNotNull($person->address);
+        self::assertInstanceOf(Address::class, $person->address);
+        self::assertEquals("Jalan Permata 1", $person->address->street);
+        self::assertEquals("Jakarta", $person->address->city);
+        self::assertEquals("Indonesia", $person->address->country);
+        self::assertEquals("13320", $person->address->postalCode);
     }
 }
