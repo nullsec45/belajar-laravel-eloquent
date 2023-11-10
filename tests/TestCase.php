b9c941de6f2fd321cfc392722c0744c035777d35
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-10 23:53:56 +0700

    m:tambah delete employees, taggables

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,11 +11,11 @@ abstract class TestCase extends BaseTestCase
 
     protected function setUp():void{
         parent::setUp();
-
+        
+        DB::delete("DELETE FROM employees");
         DB::delete("DELETE FROM persons");
         DB::delete("DELETE FROM reviews");
         DB::delete("DELETE FROM images");
-        DB::delete("DELETE FROM tags");
         DB::delete("DELETE FROM customers_likes_products");
         DB::delete("DELETE FROM products");
         DB::delete("DELETE FROM categories");
@@ -23,5 +23,7 @@ abstract class TestCase extends BaseTestCase
         DB::delete("DELETE FROM virtual_accounts");
         DB::delete("DELETE FROM wallets");
         DB::delete("DELETE FROM customers");
+        DB::delete("DELETE FROM taggables");
+        DB::delete("DELETE FROM tags");
     }
 }
