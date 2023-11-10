2bd5851ee0afe184ab89e4079676a9be2a14eb82
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-10 23:53:38 +0700

    m:tambah 1 seeder product

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -28,5 +28,13 @@ class ProductSeeder extends Seeder
         $product2->price=9000000;
         $product2->category_id="FOOD";
         $product2->save();
+
+        $product3=new Product();
+        $product3->id="3";
+        $product3->name="Product 3";
+        $product3->description="Description Product 3";
+        $product3->price=9000000;
+        $product3->category_id="MUSIC";
+        $product3->save();
     }
 }
