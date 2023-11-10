a1882092cc5d44830f014a1fefb417b29f9750f3
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-10 23:53:14 +0700

    m:tambah beberapa category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -19,5 +19,24 @@ class CategorySeeder extends Seeder
         $category->description="Food Category";
         $category->is_active=true;
         $category->save();
+
+        $category2=new Category();
+        $category2->id="FASHION";
+        $category2->name="Fashion";
+        $category2->is_active=true;
+        $category2->save();
+
+        $category3=new Category();
+        $category3->id="ELEKTRONIK";
+        $category3->name="Elektronik";
+        $category3->is_active=true;
+        $category3->save();
+
+        $category4=new Category();
+        $category4->id="MUSIC";
+        $category4->name="Music";
+        $category4->description="Music Description";
+        $category4->is_active=true;
+        $category4->save();
     }
 }
